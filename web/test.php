244a720480f254e5121f648ce8b678feb25dea0e
ariqahmansoor <ariqahhhh@gmail.com>
2022-07-09 21:55:26 +0800

    commit meta description

--- web/test.php
+++ web/test.php
@@ -3,6 +3,7 @@
     <head>
         <title>Test Hello World 2</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Hello World!">
     </head>
     <body>
     <span style="size:20px;padding:48px;margin:8px;">Hello World!<span>
