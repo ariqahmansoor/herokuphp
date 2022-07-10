31f4dbdf3ab23b17ca99205cbf1cf719145baf22
ariqahmansoor <ariqahhhh@gmail.com>
2022-07-10 16:18:29 +0800

    commit fixes

--- web/index.php
+++ web/index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Test Hello World 1</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Hello World!">
-    </head>
-    <body>
-    <span style="size:20px;padding:48px;margin:8px;">Hello World!<span>
-    </body>
+<head>
+  <title>Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Put your description here.">
+  <!-- fix: Tap targets are not sized appropriately -->
+  <style data-meta="MuiButtonBase">
+    .webchat-MuiButtonBase-root {
+        padding:48px;
+        margin:8px;
+    }
+  </style>
+</head>
+<body>
+  <!-- fix : Image elements do not have [alt] attributes -->
+  <img src="img/wallpaper.jpg" alt="view of the ocean from a mountain top">
+  <!-- fix: [user-scalable="no"] is used in the <meta name="viewport"> element or the [maximum-scale] attribute is less than 5 -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0">
+  <!-- fix : Serve images in next-gen formats. -->
+  <img src="img/wallpaper.avif" alt="view of the ocean from a mountain top">
+</body>
 </html>
